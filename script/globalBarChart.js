077ea5f62872df0a3a57660d1b428dbea64089e2
Tony Lin <tonylin1208@gmail.com>
2018-11-06 11:15:37 +0800

    optimize global bar chart

--- script/globalBarChart.js
+++ script/globalBarChart.js
@@ -1,25 +1,22 @@
-// // $.get('https://open-data-220705.appspot.com/api/global_death_rate/2016', function (e) {
-// //   console.log(e);
-  
-// // });
 
 // get barchart global data
 var globalDeathRate = "https://open-data-220705.appspot.com/api/global_death_rate/2016";
 var xhrGlobalDeathData = new XMLHttpRequest();
-var globalDeathData = [['Country', 'DeathRate']];
+var globalDeathData = [['Country', '死亡率']];
 xhrGlobalDeathData.open('GET', globalDeathRate, true);
 xhrGlobalDeathData.send();
 
 xhrGlobalDeathData.onreadystatechange = function () {
-  console.log( this.readyState);
+  // console.log( this.readyState);
   if (this.readyState === 4 && this.status === 200) {
     var apiDeathData = JSON.parse(this.responseText);
-    console.log(apiDeathData,111111111111);
+    // console.log(apiDeathData,111111111111);
     var count = 0;
     for  ( key in apiDeathData.response ) {
-      globalDeathData[count++ +1] = [key, apiDeathData.response[key]];
+      var value = apiDeathData.response[key]
+      globalDeathData[count++ +1] = [key, value];
     }
-    console.log(globalDeathData, 99999999999999);    
+    // console.log(globalDeathData, 99999999999999);    
   }
 }
 
@@ -32,10 +29,18 @@ function drawBarChart() {
 
   var options = {
     hAxis: {
-      title: '世界各國'
+      // title: '世界各國'
     },
     vAxis: {
-    }
+    },
+    chartArea: { 
+      top: 50 , 
+      weight: '90%'
+    },
+    colors: ['#ed6555'],
+    backgroundColor: '#fff7e2',
+    legend: { position: "none" },
+    fontSize: 8
   };
 
   var chart = new google.visualization.ColumnChart(
@@ -43,3 +48,8 @@ function drawBarChart() {
 
   chart.draw(data, options);
 }
+
+// // hover can change bar color
+// function createCustomHTMLContent(htmlText) {
+//   return '<span class="tooltip"><strong>' + htmlText + '</strong></div>';
+// }
