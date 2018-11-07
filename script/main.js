4f29b4b5afd8d113f13ab488d4d50fba918851ac
Tony Lin <tonylin1208@gmail.com>
2018-11-07 14:58:52 +0800

    add bmi food section

--- script/main.js
+++ script/main.js
@@ -41,3 +41,26 @@ function openLinks(e) {
 
 reference.addEventListener('click', openLinks);
 // window.addEventListener('scroll', debounce(changePage))
+
+
+// period change tab in bmi section
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+} 
