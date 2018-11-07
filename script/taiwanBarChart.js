763fd72b6d0c559d8c0499345467a284f583b427
Tony Lin <tonylin1208@gmail.com>
2018-11-07 16:16:46 +0800

    modify taiwan bar chart uri

--- script/taiwanBarChart.js
+++ script/taiwanBarChart.js
@@ -1,27 +1,20 @@
-
 // get barchart taiwan data
-var taiwanDeathRate = "http://745ff6fe.ngrok.io/api/death_rate";
+var taiwanDeathRate = "https://open-data-220705.appspot.com/api/death_rate";
 var xhrTaiwanDeathRate = new XMLHttpRequest();
 var taiwanDeathRateData = [['county', 'deathRate']];
 xhrTaiwanDeathRate.open("POST", taiwanDeathRate);
 xhrTaiwanDeathRate.setRequestHeader("Content-type", "application/json");
-xhrTaiwanDeathRate.setRequestHeader("Access-Control-Allow-Origin", "*");
-xhrTaiwanDeathRate.setRequestHeader("Access-Control-Request-Method", "POST");
 xhrTaiwanDeathRate.send(JSON.stringify({
   type: "newborn",
   year: 2017
 }));
 
 xhrTaiwanDeathRate.onreadystatechange = function () {
-  console.log(this.readyState);
-  console.log(this.status);
   if (this.readyState === 4 && this.status === 200) {
     var rowDeathRate = JSON.parse(this.responseText);
     console.log(rowDeathRate, 111111111111);
     var count = 0;
     for (key in rowDeathRate.data) {
-      console.log(key);
-      console.log(rowDeathRate.data[key]);
       taiwanDeathRateData[count++ + 1] = [key, rowDeathRate.data[key]];
     }
     console.log(taiwanDeathRateData.pop(), 99999999999999);
@@ -36,7 +29,6 @@ function drawMultSeries() {
 
   var options = {
     hAxis: {
-      // title: '世界各國'
     },
     vAxis: {
     },
