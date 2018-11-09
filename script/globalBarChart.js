692f83a65fa8fec945c4ea14a338564add059aaa
Tony Lin <tonylin1208@gmail.com>
2018-11-09 11:20:32 +0800

    finish the global radio change chart

--- script/globalBarChart.js
+++ script/globalBarChart.js
@@ -1,8 +1,20 @@
 var xhrGlobalDeathData = new XMLHttpRequest();
-var globalDeathData;
-function sendGet( year ) {
+var globalDeathData = [['Country', '死亡率']];
+
+function getGlobal() {
+  //  讀取radio的值
+  var form = document.getElementById("global__year");
+  for (var i = 0; i < form.year.length; i++) {
+    if (form.year[i].checked) {
+      var year = form.year[i].value;
+    }
+  }
+  // console.log(year)
+  return renewGlobalDraw(year);
+}
+
+function sendGet(year) {
   // get barchart global data
-  globalDeathData = [['Country', '死亡率']];
   var globalDeathRate = "https://open-data-220705.appspot.com/api/global_death_rate/" + year;
   xhrGlobalDeathData.open('GET', globalDeathRate, true);
   xhrGlobalDeathData.send();
@@ -13,65 +25,53 @@ function sendGet( year ) {
       var apiDeathData = JSON.parse(this.responseText);
       // console.log(apiDeathData,111111111111);
       var count = 0;
-      for  ( key in apiDeathData.response ) {
+      globalDeathData = [['Country', '死亡率']];
+      for (key in apiDeathData.response) {
         var value = apiDeathData.response[key]
-        globalDeathData[count++ +1] = [key, value];
+        globalDeathData[count++ + 1] = [key, value];
       }
-      console.log(globalDeathData, 99999999999999);    
+      // console.log(globalDeathData, 99999999999999);    
     }
   }
   return globalDeathData;
 }
 
-function getGlobal() {
-  //  讀取radio的值
-  var form = document.getElementById("global__year");
-  for (var i = 0; i < form.year.length; i++) {
-    if (form.year[i].checked) {
-      var year = form.year[i].value;
-      // console.log(year);
-    }
-  }
-  console.log(year)
-  return renewGlobalDraw( year );
-}
-
 // 把圖重新畫
-function renewGlobalDraw( year ) {
-  var data = sendGet( year );
-  console.log(data, "this is data");
+function renewGlobalDraw(year) {
+  var data = sendGet(year);
+  // console.log(data, "this is data");
   google.charts.load('current', { packages: ['corechart', 'bar'] });
   google.charts.setOnLoadCallback(drawGlobalBarChart(data));
   // drawMultSeries(data);
 }
 
-
-google.charts.load('current', { packages: ['corechart','bar'] });
-google.charts.setOnLoadCallback(drawGlobalBarChart);
-
-function drawGlobalBarChart(data) {
-  var data = new google.visualization.arrayToDataTable(data);
-
+function drawGlobalBarChart(setData) {
+  console.log(setData, "daadadaaadaa");
+  var data = new google.visualization.arrayToDataTable(setData);
+  console.log(data, "global hahahahaaha");
   var options = {
     hAxis: {
       // title: '世界各國'
     },
     vAxis: {
     },
-    chartArea: { 
-      top: 50 , 
+    chartArea: {
+      top: 50,
       weight: '90%'
     },
     colors: ['#ed6555'],
     backgroundColor: '#fff7e2',
-    legend: { position: "none" },
+    legend: {
+      position: "none",
+      fontSize: 19
+    },
     fontSize: 8
   };
 
   var chart = new google.visualization.ColumnChart(
     document.getElementById('chart__global'));
 
-  chart.draw(data, options);
+  chart.draw(data, google.charts.Bar.convertOptions(options));
 }
 
 // // hover can change bar color
